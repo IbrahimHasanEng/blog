6a1c1de4dc154da64effead934f352f3a581bf2b
Ibrahim Hasan <ibrahim_hasan_eng@hotmail.com>
2018-02-06 19:55:40 +0300

    The blog now depends on CDN Font-Awesome and CDN fonts. Some stylin is done as well.

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -6,9 +6,10 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
 <!-- Styles -->
-<link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
 <link rel='stylesheet' href='http://fonts.googleapis.com/earlyaccess/droidarabickufi.css'>
 <link rel='stylesheet' href='http://fonts.googleapis.com/earlyaccess/droidarabicnaskh.css'>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
 <title>نور @yield('title')</title>
+<script src="https://use.fontawesome.com/9e6c6e2548.js"></script>
